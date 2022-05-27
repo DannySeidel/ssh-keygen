ff7db316395a9a71550f7c9f0509fd1d65d4a29b
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2022-05-27 15:59:38 +0200

    updated script

--- ssh-keygen.sh
+++ ssh-keygen.sh
@@ -1,10 +1,51 @@
 echo "Generating key with: '$1'"
 ssh-keygen -t ed25519 -C "$1"
-eval "$(ssh-agent -s)"
-ssh-add --apple-use-keychain ~/.ssh/id_ed25519
-echo "Copying generated key to clipboard"
-pbcopy < ~/.ssh/id_ed25519.pub
-# add description
-echo "Opening github..."
-open https://github.com/settings/keys
 
+eval "$(ssh-agent -s)"
+
+# add ssh key dependent on os
+if [ $(uname) = "Darwin" ]
+then
+	ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+else
+	ssh-add ~/.ssh/id_ed25519
+fi
+
+unsupported=$true
+
+while [ unsupported ]
+do
+	echo "Do you want to use the github-cli to add your key (y/n): "
+	read method
+	
+	if [ $method = "y" ]
+	then
+		echo "Using github-cli to add your key..."
+		gh ssh-key add ~/.ssh/id_ed25519.pub
+
+		echo "Now you are using secure shell with your github account."
+		unsupported=$false
+
+	 elif [ $method = "n" ]
+	then
+		echo "Copying generated key to clipboard..."
+		pbcopy < ~/.ssh/id_ed25519.pub
+
+		echo "In the next step github.com will be opened."
+		echo "Past the contents of your clipboard in the textbox with the title 'key' and click 'Add SSH key'."
+		echo "Now you are using secure shell with your github account."
+
+		read -p "Press 'Enter' to continue."
+
+		echo "Opening github..."
+		if [ $(uname) = "Darwin" ]
+		then
+			open https://github.com/settings/ssh/new
+		else
+			xdg-open https://github.com/settings/ssh/new
+		fi
+		unsupported=$false
+
+	else
+		echo "Unsupported input '$method'."
+done
